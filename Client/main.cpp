35e72eebfb9dbc703196c506e93e0598f313ae2e
supamodd <ghosts.zz@yandex.ru>
2026-06-12 23:02:52 +0300

    done

--- Client/main.cpp
+++ Client/main.cpp
@@ -3,6 +3,7 @@
 #endif //!WIN32_LEAN_AND_MEAN
 
 #include<iostream>
+#include<string>
 #include<Windows.h>
 #include<WinSock2.h>
 #include<WS2tcpip.h>
@@ -12,6 +13,7 @@ using namespace std;
 #pragma comment(lib, "WS2_32.lib")	//подгружает реализации фукций из статической библиотеки для <WS2TCPIP.h>
 
 #define MTU		1500
+#define EXIT_COMMAND "exit"
 
 void main()
 {
@@ -34,7 +36,7 @@ void main()
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 
-	iResult = getaddrinfo("127.0.0.1", "27015", &hints, &target);//По символьному имени получает числовой адрес целевого узла (IP-адрес)
+	iResult = getaddrinfo("127.0.0.1", "27015", &hints, &target);
 	if (iResult != 0)
 	{
 		cout << "getaddrinfo failed with error " << iResult << endl;
@@ -42,7 +44,6 @@ void main()
 		return;
 	}
 
-	//3) Создаем сокет для подключения к Серверу:
 	SOCKET connect_socket = socket(target->ai_family, target->ai_socktype, target->ai_protocol);
 	if (connect_socket == INVALID_SOCKET)
 	{
@@ -52,7 +53,6 @@ void main()
 		return;
 	}
 
-	//4) Подключаемся к Серверу:
 	iResult = connect(connect_socket, target->ai_addr, target->ai_addrlen);
 	if (iResult != 0)
 	{
@@ -64,31 +64,61 @@ void main()
 	}
 	freeaddrinfo(target);
 
-	//5) Отправка данных Серверу:
-	CHAR send_buffer[MTU] = "Hello Server!!!";
-	iResult = send(connect_socket, send_buffer, strlen(send_buffer), NULL);
-	if (iResult == SOCKET_ERROR)
-	{
-		cout << "Send failed with error: " << WSAGetLastError() << endl;
-		closesocket(connect_socket);
-		WSACleanup();
-		return;
-	}
-	else cout << "Sent " << iResult << " Bytes" << endl;
+	cout << "Подключение к Серверу установлено." << endl;
+	cout << "Введите сообщение и нажмите Enter для отправки." << endl;
+	cout << "Для отключения от Сервера введите команду '" << EXIT_COMMAND << "'." << endl << endl;
 
-	//6) Получение данных от Сервера:
-	CHAR recv_buffer[MTU] = {/*initializer_list*/ };
-	do
+	CHAR send_buffer[MTU] = {};
+	CHAR recv_buffer[MTU] = {};
+	string user_input;
+
+	while (true)
 	{
+		cout << "Client> ";
+		if (!getline(cin, user_input))
+		{
+			break;
+		}
+
+		if (user_input.empty()) continue;
+
+		strncpy_s(send_buffer, MTU, user_input.c_str(), _TRUNCATE);
+
+		iResult = send(connect_socket, send_buffer, strlen(send_buffer), NULL);
+		if (iResult == SOCKET_ERROR)
+		{
+			cout << "Send failed with error: " << WSAGetLastError() << endl;
+			break;
+		}
+		else cout << "Sent " << iResult << " Bytes" << endl;
+
+		if (user_input == EXIT_COMMAND)
+		{
+			cout << "Отключение от Сервера по команде '" << EXIT_COMMAND << "'..." << endl;
+			break;
+		}
+
+		ZeroMemory(recv_buffer, MTU);
 		iResult = recv(connect_socket, recv_buffer, MTU, NULL);
-		if (iResult > 0)cout << recv_buffer << endl;
-		else if (iResult == 0) cout << "Nothing received from Server" << endl;
-		else cout << "Receive failed with error: " << WSAGetLastError() << endl;
-	} while (iResult > 0);
+		if (iResult > 0)
+		{
+			cout << "Server> " << recv_buffer << endl;
+		}
+		else if (iResult == 0)
+		{
+			cout << "Сервер закрыл соединение." << endl;
+			break;
+		}
+		else
+		{
+			cout << "Receive failed with error: " << WSAGetLastError() << endl;
+			break;
+		}
+	}
 
-	//7) Разрываем TCP-соединение:
+	//6) Разрываем TCP-соединение:
 	iResult = shutdown(connect_socket, SD_BOTH);
-	if (iResult != 0)cout << "shutdown failed with error " << WSAGetLastError() << endl;
+	if (iResult != 0) cout << "shutdown failed with error " << WSAGetLastError() << endl;
 
 	//?) Освобождаем ресурсы WinSOCK:
 	closesocket(connect_socket);
