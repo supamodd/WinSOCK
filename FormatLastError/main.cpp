5dcee3753c0868ff06777a3596342244d83698ef
supamodd <ghosts.zz@yandex.ru>
2026-06-25 09:46:34 +0300

    upd

--- FormatLastError/main.cpp
+++ FormatLastError/main.cpp
@@ -1,21 +1,31 @@
-#include<Windows.h>
-#include<cstdio>
+#include "FormatLastError.h"
+
+#include <cstdio>
+#include <climits>
 
 CHAR* FormatLastError(CHAR szBuffer[], DWORD dwError)
 {
-	LPSTR lpError;
-	FormatMessage
-	(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dwError,
-		MAKELANGID(LANG_NEUTRAL, LANG_SYSTEM_DEFAULT),
-		(LPSTR)&lpError,
-		NULL,
-		NULL
-	);
-	//cout << lpError << endl;
-	sprintf(szBuffer, "Error %i:%s", dwError, lpError);
-	LocalFree(lpError);
-	return szBuffer;
+    LPSTR lpError = nullptr;
+
+    DWORD charsWritten = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        nullptr,
+        dwError,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        reinterpret_cast<LPSTR>(&lpError),
+        0,
+        nullptr
+    );
+
+    if (charsWritten == 0 || lpError == nullptr)
+    {
+        sprintf_s(szBuffer, USHRT_MAX + 1, "Error %lu", dwError);
+        return szBuffer;
+    }
+
+    sprintf_s(szBuffer, USHRT_MAX + 1, "Error %lu: %s", dwError, lpError);
+    LocalFree(lpError);
+    return szBuffer;
 }
